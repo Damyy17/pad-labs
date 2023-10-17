25b76f044ea7adf7f22522a49d2c4e780ae4d1ce
Damyy17 <90212650+Damyy17@users.noreply.github.com>
2023-10-18 00:48:23 +0300

    route changes, required fields and unique inteeractions

--- cma-microservice/src/routes/interactions.js
+++ cma-microservice/src/routes/interactions.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const Interaction = require('../models/interactionModel');
+const CommentInteraction = require('../models/commentInteractionModel')
 
 // handling logging when a user views a piece of content
 router.post('/interactions/view', async (req, res) => {
-  const { userId, contentId, timestamp } = req.body;
+  const { userId, contentId} = req.body;
 
   try {
     const interaction = new Interaction({
       userId,
       contentId,
-      timestamp,
-      type: 'view' 
+      timestamp: Date.now(),
+      interactionType: 'view' 
     });
     await interaction.save();
     return res.json({ message: 'View interaction logged successfully' });
@@ -20,60 +21,59 @@ router.post('/interactions/view', async (req, res) => {
   }
 });
 
+// handling logging when a user likes a piece of content
 router.post('/interactions/like', async (req, res) => {
-  const { userId, contentId, timestamp } = req.body;
+  const { userId, contentId} = req.body;
 
-  const interaction = new Interaction({
-    userId,
-    contentId,
-    timestamp,
-    type: 'like'
-  });
-
-  await interaction.save((err) => {
-    if (err) {
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const interaction = new Interaction({
+      userId,
+      contentId,
+      timestamp: Date.now(),
+      interactionType: 'like'
+    });
+    await interaction.save();
     return res.json({ message: 'Like interaction logged successfully' });
-  });
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
+// handling logging when a user comments on a piece of content
 router.post('/interactions/comment', async (req, res) => {
-  const { userId, contentId, comment, timestamp } = req.body;
+  const { userId, contentId, comment} = req.body;
 
-  const interaction = new Interaction({
-    userId,
-    contentId,
-    comment,
-    timestamp,
-    type: 'comment'
-  });
-
-  await interaction.save((err) => {
-    if (err) {
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const interaction = new CommentInteraction({
+      userId,
+      contentId,
+      comment,
+      timestamp: Date.now(),
+      interactionType: 'comment'
+    });
+    await interaction.save();
     return res.json({ message: 'Comment interaction logged successfully' });
-  });
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-router.post('/interactions/add-to-playlist', async (req, res) => {
-  const { userId, contentId, playlistId, timestamp } = req.body;
+// handling logging when a user likes a piece of content
+router.post('/interactions/add-to-favorites', async (req, res) => {
+  const { userId, contentId } = req.body;
 
-  const interaction = new Interaction({
-    userId,
-    contentId,
-    playlistId,
-    timestamp,
-    type: 'add-to-playlist'
-  });
-
-  await interaction.save((err) => {
-    if (err) {
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-    return res.json({ message: 'Add-to-playlist interaction logged successfully' });
-  });
+  try {
+    const interaction = new Interaction({
+      userId,
+      contentId,
+      timestamp: Date.now(),
+      interactionType: 'add-to-favorites'
+    });
+    await interaction.save();
+    return res.json({ message: 'Add-to-favorites interaction logged successfully' });
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 
